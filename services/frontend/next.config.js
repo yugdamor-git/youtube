2ef3626a8f9b3f7b2657cad67f01bd814efb72c2
Yug Damor <yugdamor.dev@gmail.com>
2022-05-10 20:11:06 +0530

    done

---
+++ services/frontend/next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const { i18n } = require("./next-i18next.config");
+const nextConfig = {
+  reactStrictMode: false,
+  i18n,
+  env: {
+    BASE_DOMAIN: "https://ytshorts.savetube.me",
+  },
+};
+
+module.exports = nextConfig;
