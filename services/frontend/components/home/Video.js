df8f7fcfe6fd4046e6389c0d084c56b024fcc0d3
Yug Damor <yugdamor.dev@gmail.com>
2022-05-12 09:58:07 +0530

    done

--- services/frontend/components/home/Video.js
+++ services/frontend/components/home/Video.js
@@ -146,7 +146,7 @@ const Video = ({currentVideoData,contentType,btnsp,showDownloadp}) => {
           <button
             disabled
             type="button"
-            className="text-white bg-yellow-400  focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 inline-flex items-left"
+            className="text-white bg-yellow-400  focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 inline-flex items-center"
           >
             <svg
               role="status"
